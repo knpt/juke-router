12a60b1a870005763f31a0594ed6bcf1e65e5609
Kimberly Ton <kimberlyton@gmail.com>
2017-08-22 18:32:58 -0400

    up to using match.parms

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {HashRouter, Route} from 'react-router-dom';
+
 import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
@@ -9,33 +10,13 @@ export default class Main extends Component {
 
   constructor (props) {
     super(props);
-    this.state = {
-      albums: [],
-      selectedAlbum: {}
-    };
-    this.selectAlbum = this.selectAlbum.bind(this);
-    this.deselectAlbum = this.deselectAlbum.bind(this);
+    
+   
   }
 
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
 
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
 
-  deselectAlbum () {
-    this.setState({ selectedAlbum: {}});
-  }
+
 
   render () {
     return (
@@ -43,13 +24,26 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        <HashRouter>
+          <div className="col-xs-10">
+            <Route
+              exact
+              path='/'
+              component = {AllAlbums}
+            />
+
+            <Route 
+              exact
+              path='/albums'
+              component={AllAlbums}
+            />
+
+            <Route
+              path='/albums/:albumId'
+              component={SingleAlbum}
+            />
+          </div>
+        </HashRouter>
         <Player />
       </div>
     );
--- browser/react/components/SingleAlbum.js
+++ browser/react/components/SingleAlbum.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Songs from '../components/Songs';
+import axios from 'axios'
+
 
 export default class SingleAlbum extends Component {
+  constructor(){
+    super()
+    this.state = {
+      selectedAlbum: {}
+    }
+  }
+
+  componentDidMount () {
+    const albumId = this.props.match.params.albumId
+
+    axios.get(`/api/albums/${albumId}`)
+      .then(res => res.data)
+      .then(album => {
+        this.setState({selectedAlbum: album})
+    });
+  }
 
   render () {
 
-    const album = this.props.album;
-
+    const album = this.state.selectedAlbum;
+    console.log(this.props.match.params)
     return (
       <div className="album">
         <div>
